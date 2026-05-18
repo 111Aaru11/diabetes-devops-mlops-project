b74bcad0537de3cff37dcf164205481cc1fcd194
aarushi <aaarushi817@gmail.com>
2026-05-18 11:12:27 +0530

    Added Terraform Docker setup

---
+++ terraform/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.1"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "diabetes_app" {
+  name = "aarushi1111/diabetes-app:latest"
+}
+
+resource "docker_container" "diabetes_container" {
+  image = docker_image.diabetes_app.image_id
+  name  = "terraform-diabetes-container"
+
+  ports {
+    internal = 5000
+    external = 5001
+  }
+}
