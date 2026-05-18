1df79d9ade99fde5495df4ea6226ed14226c8f0c
aarushi <aaarushi817@gmail.com>
2026-05-18 11:20:30 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,12 +2,14 @@ terraform {
   required_providers {
     docker = {
       source  = "kreuzwerker/docker"
-      version = "~> 3.0.1"
+      version = "3.0.2"
     }
   }
 }
 
-provider "docker" {}
+provider "docker" {
+    host = "npipe:////./pipe/docker_engine"
+}
 
 resource "docker_image" "diabetes_app" {
   name = "aarushi1111/diabetes-app:latest"
